06e3661e55edebd154afb19852fec7d57a20ccee
igorluchenkov <iluchenkov@gmail.com>
2018-10-02 23:29:14 +0300

    refactoring & better project structure

--- routes/profile.js
+++ routes/profile.js
@@ -8,4 +8,8 @@ router.get('/', auth, async (req, res) => {
 	res.send(user)
 })
 
+router.post('/change-password', auth, async (req, res) => {
+	// const user = await User.findById(req.user._id)
+})
+
 module.exports = router
