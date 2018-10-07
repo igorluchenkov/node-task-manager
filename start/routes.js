819d047c01fb11b36d15eedafc9e2689e142d6f2
igorluchenkov <iluchenkov@gmail.com>
2018-10-07 21:40:51 +0300

    rename error middleware

--- start/routes.js
+++ start/routes.js
@@ -2,12 +2,12 @@ const express = require('express')
 const tasksRoutes = require('../routes/tasks')
 const authRoutes = require('../routes/auth')
 const profileRoutes = require('../routes/profile')
-const error = require('../middlewares/error')
+const errorHandler = require('../middlewares/errorHandler')
 
 module.exports = app => {
 	app.use(express.json())
 	app.use('/tasks/', tasksRoutes)
 	app.use('/auth/', authRoutes)
 	app.use('/profile/', profileRoutes)
-	app.use(error)
+	app.use(errorHandler)
 }
