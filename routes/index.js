f6247c9cc417265ecc316a80ea25c3d3fad4e5c0
igor <iluchenkov@gmail.com>
2018-10-18 01:04:16 +0300

    refactoring

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,6 @@ const UsersController = require('../controllers/UsersController')
 // Middlewares.
 const userCanManageTask = require('../middlewares/userCanManageTask')
 const userCanReadTasks = require('../middlewares/userCanReadTasks')
-const userHasAuthToken = require('../middlewares/userHasAuthToken')
 const userIsLogged = require('../middlewares/userIsLogged')
 const userHasAuthToken = require('../middlewares/userHasAuthToken')
 const userIsAdmin = require('../middlewares/userIsAdmin')
@@ -21,23 +20,23 @@ router.post('/profile/login', AuthController.login)
 
 // Profile.
 router.get('/profile/', [userHasAuthToken], ProfileController.getInfo)
+router.post('/profile/forget', ProfileController.forgetPassword)
 router.put('/profile/name', [userHasAuthToken, userIsLogged], ProfileController.changeName)
 router.put('/profile/email', [userHasAuthToken, userIsLogged], ProfileController.changeEmail)
 router.put('/profile/password', [userHasAuthToken, userIsLogged], ProfileController.changePassword)
-router.post('/profile/forget', ProfileController.forgetPassword)
 
 // Tasks.
 router.get('/tasks/', [userHasAuthToken, userCanReadTasks], TasksController.getList)
 router.get('/tasks/:id', [userHasAuthToken, userCanReadTasks], TasksController.get)
-router.post('/tasks/', [userHasAuthToken], TasksController.add);
-router.post('/tasks/:id/share', [userHasAuthToken, userCanManageTask], TasksController.share);
-router.put('/tasks/:id', [userHasAuthToken, userCanManageTask], TasksController.put);
-router.delete('/tasks/:id', [userHasAuthToken, userCanManageTask], TasksController.remove);
+router.post('/tasks/', [userHasAuthToken], TasksController.add)
+router.post('/tasks/:id/share', [userHasAuthToken, userCanManageTask], TasksController.share)
+router.put('/tasks/:id', [userHasAuthToken, userCanManageTask], TasksController.put)
+router.delete('/tasks/:id', [userHasAuthToken, userCanManageTask], TasksController.remove)
 
 // Users.
 router.get('/users/', [userHasAuthToken], UsersController.getList)
 router.get('/users/:id', [userHasAuthToken], UsersController.get)
 router.put('/users/:id', [userHasAuthToken, userIsAdmin], UsersController.put)
-router.delete('/users/:id', [userHasAuthToken, userIsAdmin], UsersController.remove);
+router.delete('/users/:id', [userHasAuthToken, userIsAdmin], UsersController.remove)
 
 module.exports = router
