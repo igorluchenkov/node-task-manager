c2eb2c561f07f1927a74994310895c4e83208b43
igor <iluchenkov@gmail.com>
2018-10-17 15:17:23 +0300

    wrong routes fix

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router()
 const AuthController = require('../controllers/AuthController')
 const ProfileController = require('../controllers/ProfileController')
 const TasksController = require('../controllers/TasksController')
-const UserController = require('../controllers/UsersController')
+const UsersController = require('../controllers/UsersController')
 
 // Middlewares.
 const userCanManageTask = require('../middlewares/userCanManageTask')
@@ -16,23 +16,23 @@ const userHasAuthToken = require('../middlewares/userHasAuthToken')
 const userIsAdmin = require('../middlewares/userIsAdmin')
 
 // Auth.
-router.get('/auth/', [userHasAuthToken], ProfileController.getInfo)
-router.put('/auth/password', [userHasAuthToken, userIsLogged], ProfileController.changePassword)
-
-// Profile.
-router.put('/profile/name', [userHasAuthToken, userIsLogged], ProfileController.changeName)
-router.put('/profile/email', [userHasAuthToken, userIsLogged], ProfileController.changeEmail)
-router.post('/profile/forget', ProfileController.forgetPassword)
 router.post('/profile/register', AuthController.register)
 router.post('/profile/login', AuthController.login)
+
+// Profile.
+router.get('/profile/', [userHasAuthToken], ProfileController.getInfo)
+router.put('/profile/name', [userHasAuthToken, userIsLogged], ProfileController.changeName)
+router.put('/profile/email', [userHasAuthToken, userIsLogged], ProfileController.changeEmail)
+router.put('/profile/password', [userHasAuthToken, userIsLogged], ProfileController.changePassword)
+router.post('/profile/forget', ProfileController.forgetPassword)
 
 // Tasks.
 router.get('/tasks/', [userHasAuthToken, userCanReadTasks], TasksController.getList)
 router.get('/tasks/:id', [userHasAuthToken, userCanReadTasks], TasksController.get)
 router.post('/tasks/', [userHasAuthToken], TasksController.add);
+router.post('/tasks/:id/share', [userHasAuthToken, userCanManageTask], TasksController.share);
 router.put('/tasks/:id', [userHasAuthToken, userCanManageTask], TasksController.put);
 router.delete('/tasks/:id', [userHasAuthToken, userCanManageTask], TasksController.remove);
-router.post('/tasks/:id/share', [userHasAuthToken, userCanManageTask], TasksController.share);
 
 // Users.
 router.get('/users/', [userHasAuthToken], UsersController.getList)
