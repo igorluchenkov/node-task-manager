637d76e4684e6a496540e91e860989466d1d7ccb
igor <iluchenkov@gmail.com>
2018-10-17 15:06:59 +0300

    refactoring

--- start/routes.js
+++ start/routes.js
@@ -1,17 +1,9 @@
 const express = require('express')
-const tasksRoutes = require('../routes/tasks')
-const authRoutes = require('../routes/auth')
-const profileRoutes = require('../routes/profile')
-const usersRoutes = require('../routes/users')
-const apiRoutes = require('../routes')
+const routes = require('../routes')
 const errorHandler = require('../middlewares/errorHandler')
 
 module.exports = app => {
 	app.use(express.json())
-	app.use('/', apiRoutes)
-	// app.use('/tasks/', tasksRoutes)
-	// app.use('/auth/', authRoutes)
-	// app.use('/profile/', profileRoutes)
-	// app.use('/users/', usersRoutes)
+	app.use('/', routes)
 	app.use(errorHandler)
 }
